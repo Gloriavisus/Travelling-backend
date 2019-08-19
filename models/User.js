02f344e7b3dbe2a9d00ef7feb8f7da789fe4c0cb
Gloria Visus <gloriavisus@gmail.com>
2019-08-19 15:10:07 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  hobbies:{
+    type: String,
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  preference: {
+    type: String,
+  },
+
+  description: {
+    type: String,
+  },
+
+  image:{
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
